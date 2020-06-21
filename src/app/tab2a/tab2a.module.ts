5057f83d5b46979a056894ffec2b95eecd9adc4c
Leedabord <gwforumlakers@gmail.com>
2020-06-21 14:12:13 -0700

    3 files modified

--- src/app/tab2a/tab2a.module.ts
+++ src/app/tab2a/tab2a.module.ts
@@ -3,15 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { Tab2aPage } from './tab2a.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: '', component: Tab2aPage }])
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2aPage]
 })
-export class Tab2PageModule {}
+export class Tab2aPageModule {}
