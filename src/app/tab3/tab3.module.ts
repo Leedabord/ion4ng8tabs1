e15b23fc50c5e685bc08f3bb97043c02f1516bc5
Leedabord <gwforumlakers@gmail.com>
2020-06-27 20:34:07 -0700

    4 files modified

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,17 +1,27 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Tab3Page,
+  }
+];
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [Tab3Page]
 })
 export class Tab3PageModule {}
+
+/* */
